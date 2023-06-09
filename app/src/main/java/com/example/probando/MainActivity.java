dd1ea935191fa3287e402cdd5120c441daf7d284
Yolima Rojas <yolimarojasmoreno05@gmail.com>
2023-06-09 12:46:37 -0500

    ahora mi MainActivity no tiene codigo

--- app/src/main/java/com/example/probando/MainActivity.java
+++ app/src/main/java/com/example/probando/MainActivity.java
@@ -10,23 +10,4 @@ import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtNombre, txtEmail, txtDomicilio;
-    Button btnGuardar;
-    Spinner cbxDeptos;
-    RadioButton rbCompleto, rbMedio;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        txtNombre = findViewById(R.id.txtNombre);
-        txtEmail = findViewById(R.id.editTextTextEmailAddress);
-        txtDomicilio = findViewById(R.id.textView);
-        txtNombre = findViewById(R.id.txtNombre);
-        txtNombre = findViewById(R.id.txtNombre);
-        txtNombre = findViewById(R.id.txtNombre);
-        txtNombre = findViewById(R.id.txtNombre);
-
-    }
 }
